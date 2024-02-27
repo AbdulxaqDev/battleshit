74d881e7e4d02c1a1f216c855292c626a870d7ad
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-27 23:04:35 +0300

    feat: set up some requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,15 @@
 import { WebSocketServer } from "ws";
-import { playerDataToJSON, register } from "./utils/login.util";
+
+import {
+  addUserToRoomRequest,
+  createRoomRequest,
+  regRequest,
+  updateRoomRequest,
+  createGameRequest,
+} from "./requests/requests";
+import { roomsDB } from "./db/db";
+import { removeFullRoom } from "./utils/room.util";
+import { reqLogger, request } from "./helpers/helpers";
 
 const PORT = 3000;
 
@@ -12,13 +22,39 @@ wss.on("listening", () => {
 wss.on("connection", (ws) => {
   ws.on("error", console.error);
 
-  ws.on("message", (data) => {
-    const player = playerDataToJSON(data.toString());
+  ws.on("message", (message) => {
+    const { type, data } = request(message);
 
-    if (player.type === "reg") {
-      const registered = register(player);
-      ws.send(registered);
+    switch (type) {
+      case "reg":
+        regRequest(ws, data);
+        updateRoomRequest(ws);
+        break;
+      case "create_room":
+        createRoomRequest(ws, data);
+        updateRoomRequest(ws);
+        break;
+      case "add_user_to_room":
+        const roomIndex = data.indexRoom;
+        addUserToRoomRequest(ws, roomIndex);
+        updateRoomRequest(ws);
+        createGameRequest(ws);
+        removeFullRoom(roomIndex);
+        console.log(
+          JSON.stringify(
+            roomsDB.find((room) => room.roomId === data.indexRoom),
+            null,
+            4
+          )
+        );
+        break;
+      case "add_ships":
+        break;
+      case "start_game":
+        break;
+      default:
+        break;
     }
-    
+    reqLogger(data, type);
   });
 });
