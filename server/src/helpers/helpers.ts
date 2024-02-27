0f4477d677827b021d2e2dd4b872eb181c57fb33
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-27 23:03:53 +0300

    feat: add custom helpers

--- server/src/helpers/helpers.ts
+++ server/src/helpers/helpers.ts
@@ -1,16 +1,50 @@
-import { playersDB } from "src/db/db";
-import { tPlayer } from "src/types/types";
+import { RawData, WebSocket } from "ws";
 
-export function checkUser(player: tPlayer) {
-  const { name, password } = player.data;
+import { playersDB, roomsDB } from "src/db/db";
+import { tDefault, tPlayer, tRoom, tGame } from "src/types/types";
 
-  return playersDB.find(
-    (player) => player.data.name === name && player.data.password === password
-  );
+export function isPlayerExist(ws: WebSocket) {
+  return playersDB.find((p) => p.ws === ws);
 }
 
-export function addPlayer(newPlayer: tPlayer) {
+export function addPlayer(newPlayer: tPlayer): void {
   playersDB.push(newPlayer);
-  console.log(newPlayer);
-  console.log(playersDB);
+}
+
+export function reqLogger(data: tDefault, type: string) {
+  console.log("\n");
+  console.log(
+    "\x1b[33m%s\x1b[0m\x1b[32m(%s)\x1b[0m\x1b[33m%s\x1b[0m",
+    "REQUEST",
+    type,
+    ":"
+  );
+  console.log("\x1b[36mdata: %s\x1b[0m", JSON.stringify(data, null, 4)); //cyan
+  console.log("\n");
+}
+
+export function request(DATA: RawData) {
+  const rawDATA = JSON.parse(DATA.toString());
+  return {
+    ...rawDATA,
+    data: JSON.parse(rawDATA.data || '""'),
+  };
+}
+
+export function response(
+  DATA: tPlayer | tRoom | tRoom[] | tGame,
+  type: string
+): string {
+  const stringData = {
+    type,
+    data: JSON.stringify(DATA),
+    id: 0,
+  };
+  return JSON.stringify(stringData);
+}
+
+export function isPlayerInRoom(ws: WebSocket) {
+  return roomsDB.find((room) => {
+    return room.roomUsers.find((user) => user.ws === ws);
+  });
 }
