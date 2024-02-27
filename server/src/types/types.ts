24674003484527016ddbe7e1b77625786e04e7c5
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-27 23:05:07 +0300

    feat: add custom types

--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -1,11 +1,35 @@
-export type tPlayer = {
+import { WebSocket } from "ws";
+
+export type tDefault = {
   type: string;
-  data: {
-    name: string;
-    password?: string;
-    index?: string;
-    error?: boolean;
-    errorText?: string;
-  };
+  data: string | object;
   id: number;
 };
+
+export type tPlayer = {
+  name: string;
+  password?: string;
+  index: number | string;
+  error?: boolean;
+  errorText?: string;
+  ws?: WebSocket;
+};
+
+export type tRoomPlayer = {
+  name: string;
+  index: number | string | undefined;
+  ws: WebSocket;
+};
+
+export type tRoom = {
+  roomId: number | string;
+  roomUsers: tRoomPlayer[];
+  ws: WebSocket;
+};
+
+export type tGame = {
+  idGame: string;
+  idPlayer: number | string | undefined;
+};
+
+export type tRooms = tRoom[] | string;
