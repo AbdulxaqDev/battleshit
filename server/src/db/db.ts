2c0ada6a47f25a1089b0a57b81318798063b104a
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-28 09:24:58 +0300

    feat: add winners and playes' ships DB

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,5 +1,9 @@
-import { tPlayer, tRoom } from "src/types/types";
+import { tPlayer, tRoom, tPlayersShips, tWinner } from "src/types/types";
 
 export const playersDB: tPlayer[] = [];
 
+export const winnersDB: tWinner[] = [];
+
 export const roomsDB: tRoom[] = [];
+
+export const playersShipsDB: tPlayersShips[] = [];
