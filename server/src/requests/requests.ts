0d5217d6c4962c035e30c003ff1a586f9354b50f
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-29 13:56:09 +0300

    feat: implement add ships and start game requests

--- server/src/requests/requests.ts
+++ server/src/requests/requests.ts
@@ -1,9 +1,9 @@
 import { WebSocket } from "ws";
 import { v4 as uuidv4 } from "uuid";
 
-import { roomsDB } from "src/db/db";
+import { playersShipsDB, roomsDB, winnersDB } from "src/db/db";
 import { register } from "src/utils/login.util";
-import { tGame, tPlayer, tRoom } from "src/types/types";
+import { tGame, tIncomingPlayerShips, tPlayer, tRoom } from "src/types/types";
 import { isPlayerInRoom, response } from "src/helpers/helpers";
 import { addPlayerToRoom, createRoom } from "src/utils/room.util";
 
@@ -16,18 +16,28 @@ export function regRequest(ws: WebSocket, data: tPlayer) {
 // create_room
 export function createRoomRequest(ws: WebSocket, data: tRoom) {
   if (isPlayerInRoom(ws)) return;
-  console.log("create room");
 
   createRoom(ws, data);
   addPlayerToRoom(ws);
 }
 
 // update_room
-export function updateRoomRequest(ws: WebSocket): void {
+export function updateRoomRequest(
+  ws: WebSocket,
+  roomIndex?: string | undefined
+): void {
   const rooms = roomsDB.filter(
     (room) => room.roomUsers.length < 2 && room.roomUsers.length > 0
   );
-  ws.send(response(rooms, "update_room"));
+
+  if (roomIndex) {
+    const gameRoom = roomsDB.find((room) => room.roomId === roomIndex);
+    gameRoom?.roomUsers.map((user) =>
+      user.ws.send(response(rooms, "update_room"))
+    );
+  } else {
+    ws.send(response(rooms, "update_room"));
+  }
 }
 
 // add_user_to_room
@@ -41,11 +51,66 @@ export function createGameRequest(ws: WebSocket) {
     (room) => room.roomUsers[0].ws === ws || room.roomUsers[1].ws === ws
   );
 
+  const idGame = uuidv4();
+
   gameRoom?.roomUsers.map((user) => {
     const gameID: tGame = {
-      idGame: uuidv4(),
+      idGame,
       idPlayer: user.index,
     };
     user.ws.send(response(gameID, "create_game"));
   });
+}
+
+// update_winners *TODO*
+export function updateWinnersReqeuest(ws: WebSocket) {
+  ws.send(response(winnersDB, "update_winners"));
+}
+
+// add_ships
+export function addShipsRequest(
+  ws: WebSocket,
+  playerShips: tIncomingPlayerShips
+): void {
+  const { ships, indexPlayer, gameId } = playerShips;
+  const game = playersShipsDB.find(
+    (game) => game.gameId === playerShips.gameId
+  );
+  if (game) {
+    game.players.push({
+      ships,
+      indexPlayer,
+      ws: ws,
+    });
+  } else {
+    playersShipsDB.push({
+      gameId,
+      players: [
+        {
+          ships,
+          indexPlayer,
+          ws: ws,
+        },
+      ],
+    });
+  }
+}
+
+// start_game
+export function startGameRequest(gameId: string): void {
+  const game = playersShipsDB.find((game) => game.gameId === gameId);
+  console.log("STARTING GAME 11");
+
+  if (game && game.players.length === 2) {
+    console.log("STARTING GAME");
+
+    game.players.map((player) =>
+      player.ws.send(
+        response(
+          { ships: player.ships, currentPlayerIndex: player.indexPlayer },
+          "start_game"
+        )
+      )
+    );
+  }
 }
