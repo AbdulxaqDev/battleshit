0933ecb7700216321717ce5aba188f184c364984
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-03-04 09:33:10 +0300

    feat: add custom helper function

--- server/src/helpers/helpers.ts
+++ server/src/helpers/helpers.ts
@@ -1,15 +1,7 @@
 import { RawData, WebSocket } from "ws";
 
 import { playersDB, roomsDB } from "src/db/db";
-import {
-  tDefault,
-  tPlayer,
-  tRoom,
-  tGame,
-  tWinner,
-  tPlayersShips,
-  tPlayerShips,
-} from "src/types/types";
+import { tDefault, tPlayer, tShip } from "src/types/types";
 
 export function isPlayerExist(ws: WebSocket) {
   return playersDB.find((p) => p.ws === ws);
@@ -39,17 +31,7 @@ export function request(DATA: RawData) {
   };
 }
 
-export function response(
-  DATA:
-    | tPlayer
-    | tRoom
-    | tRoom[]
-    | tGame
-    | tWinner[]
-    | tPlayersShips
-    | tPlayerShips,
-  type: string
-): string {
+export function response<T>(DATA: T, type: string): string {
   const stringData = {
     type,
     data: JSON.stringify(DATA),
@@ -62,4 +44,106 @@ export function isPlayerInRoom(ws: WebSocket) {
   return roomsDB.find((room) => {
     return room.roomUsers.find((user) => user.ws === ws);
   });
+}
+
+export function coordinatedShips(ships: tShip[]) {
+  return ships.map((ship) => {
+    const { direction, length } = ship;
+    const { x, y } = ship.position;
+    let shipCoords;
+    let shipAroundCoords;
+
+    if (length > 1) {
+      if (direction) {
+        // true => vertical |
+        shipAroundCoords = [
+          { x: x, y: y - 1 }, // top
+          { x: x - 1, y: y - 1 }, // top left side
+          { x: x + 1, y: y - 1 }, // top right side
+          { x: x - 1, y }, // left side
+          { x: x + 1, y }, // right side
+          { x: x - 1, y: y + length }, // bottom left side
+          { x: x + 1, y: y + length }, // bottom right side
+          { x: x, y: y + length }, // bottom
+        ];
+        shipCoords = [{ x, y }];
+
+        for (let i = 1; i < length; i++) {
+          shipCoords.push({
+            x,
+            y: y + i,
+          });
+          shipAroundCoords.push(
+            {
+              x: x - 1,
+              y: y + i,
+            },
+            {
+              x: x + 1,
+              y: y + i,
+            }
+          );
+        }
+      } else {
+        // fasle => horizontal __
+        shipAroundCoords = [
+          { x: x, y: y - 1 }, // top
+          { x: x - 1, y: y - 1 }, // top left side
+          { x: x - 1, y }, // left side
+          { x: x - 1, y: y + 1 }, // bottom left side
+          { x: x + length, y: y - 1 }, // right top side
+          { x: x + length, y }, // right
+          { x: x + length, y: y + 1 }, // right bottom side
+          { x, y: y + 1 }, // bottom
+        ];
+        shipCoords = [{ x, y }];
+
+        for (let i = 1; i < length; i++) {
+          shipCoords.push({
+            x: x + i,
+            y: y,
+          });
+          shipAroundCoords.push(
+            {
+              x: x + i,
+              y: y - 1,
+            },
+            {
+              x: x + i,
+              y: y + 1,
+            }
+          );
+        }
+      }
+    } else {
+      shipAroundCoords = [
+        { x: x, y: y - 1 }, // top
+        { x: x - 1, y: y - 1 }, // top left side
+        { x: x - 1, y: y }, // left side
+        { x: x - 1, y: y + 1 }, // left bottom side
+        { x, y: y + 1 }, // bottom
+        { x: x + length, y: y + 1 }, // bottom right side
+        { x: x + 1, y }, // right side
+        { x: x + length, y: y - 1 }, // top right side
+      ];
+      shipCoords = [{ x, y }];
+    }
+    return { ...ship, shipCoords, shipAroundCoords };
+  });
+}
+
+export function attackResponce(
+  x: number,
+  y: number,
+  currentPlayer: string | number | undefined,
+  status: string
+) {
+  return {
+    position: {
+      x,
+      y,
+    },
+    currentPlayer,
+    status,
+  };
 }
